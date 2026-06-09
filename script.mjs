2b7fe9fba4fd7e60e251053dc42a1b8d4b1e36a7
Fayaz <fayaz.sadat551@yahoo.com>
2026-06-09 23:50:55 +0100

    initialize script and populate user dropdown
    - Import required data and calculation utilities
    - Select user picker and stats display DOM elements
    - Dynamically generate dropdown options for each available user ID.

--- script.mjs
+++ script.mjs
@@ -1,11 +1,12 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
+import { getUserIDs, getListenEvents, getSong } from "./data.mjs";
+import { calculateStats } from "./common.mjs";
 
-import { countUsers } from "./common.mjs";
+const picker = document.getElementById("user-picker");
+const display = document.getElementById("stats-display");
 
-window.onload = function () {
-  document.querySelector("body").innerText = `There are ${countUsers()} users`;
-};
+getUserIDs().forEach(id => {
+    const opt = document.createElement("option");
+    opt.value = id;
+    opt.textContent = `User ${id}`;
+    picker.appendChild(opt);
+});
